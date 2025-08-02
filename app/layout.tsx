661ccf685d49b0d18f4bd577f06d6ce396070ee2
Daantaa <d2nutn@gmail.com>
2025-08-02 20:02:26 +0300

    Implemented adding issue feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
+import "@radix-ui/themes/styles.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
+import "./theme-config.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -26,10 +24,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
-        <NavBar />
-        <main>{children}</main>
+        <Theme appearance="light" accentColor="violet">
+          <NavBar />
+            <main className="p-5">{children}</main>
+        </Theme>
       </body>
     </html>
   );
